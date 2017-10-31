0b61e288b7eec5efeb3bad1547e3558b1b23f09b
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-10-31 15:48:08 -0400

    Fixed hasOwnProperty

--- src/Search.js
+++ src/Search.js
@@ -201,7 +201,7 @@ Search.prototype.toString = function () {
 
 Search.prototype.set = function (opt) {
   for (var k in opt) {
-    if (opt.hasOwnProperty[k]) {
+    if (opt.hasOwnProperty(k)) {
       this[typeof this[k] === "function" ? "_" + k : k] = opt[k];
     }
   }
